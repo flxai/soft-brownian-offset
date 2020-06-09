c125d66145da91eefd71e28c8c9cdd94076d5dd6
Felix <git@flx.ai>
2020-06-09 19:51:56 +0200

    Add CI

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+import os
+
+github_url = f'https://github.com/{os.environ.get("GITHUB_REPOSITORY")}'
+
+setup(
+    setup_requires=['pbr'],
+    pbr=True,
+    tests_require=['nose'],
+    test_suite='nose.collector',
+    project_urls={
+        'Source Code': github_url,
+        'Bug Tracker': f'{github_url}/issues',
+        'Documentation': 'https://sbo.readthedocs.io',
+    },
+)
