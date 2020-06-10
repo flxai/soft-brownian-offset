ec64cdd36ed8fb37f9bac94685da22aa7e5903a4
Felix <git@flx.ai>
2020-06-10 02:02:25 +0200

    Revert "Fix imports"
    This reverts commit e22ce4497b138037313d7e608bf58eaf12794bb1.

--- sbo/__init__.py
+++ sbo/__init__.py
@@ -2,3 +2,5 @@ from pbr.version import VersionInfo
 
 __version__ = VersionInfo('sbo').version_string()
 __version_info__ = VersionInfo('sbo').semantic_version().version_tuple()
+
+from .sbo import soft_brownian_offset, gaussian_hyperspheric_offset
