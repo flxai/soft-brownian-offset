c65a75bfc002e7f774a48a3ba53703cf9b818676
Felix <git@flx.ai>
2021-02-25 00:33:30 +0100

    Add Sphinx lib

--- docs/conf.py
+++ docs/conf.py
@@ -37,6 +37,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.coverage',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.autosectionlabel',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
